7417e8dea2370aefc33fffb22475bccb988c4dab
Shilo Gilor <sgilor@wiser.com>
2018-11-21 11:14:16 +0200

    new task

---
+++ task_2/src/Coords/MyCoords.java
@@ -0,0 +1,47 @@
+package Coords;
+
+import Geom.Point3D;
+
+public class MyCoords implements coords_converter
+{
+	@Override
+	public Point3D add(Point3D gps, Point3D local_vector_in_meter) {
+		Point3D cartesian= gps.convert_radians_to_cartesian(gps);
+		cartesian.add(local_vector_in_meter);
+		gps = cartesian.convert_cartesian_to_radians(cartesian);
+		return gps;
+	}
+
+	@Override
+	public double distance3d(Point3D gps0, Point3D gps1) 
+	{		
+		Point3D cartesian0= gps0.convert_radians_to_cartesian(gps0);
+		Point3D cartesian1= gps0.convert_radians_to_cartesian(gps1);
+		return cartesian1.distance3D(cartesian0);
+	}
+
+	@Override
+	public Point3D vector3D(Point3D gps0, Point3D gps1) 
+	{
+		Point3D cartesian0= gps0.convert_radians_to_cartesian(gps0);
+		Point3D cartesian1= gps0.convert_radians_to_cartesian(gps1);
+		cartesian1.add(-cartesian0.x(), -cartesian0.y(), -cartesian0.z());
+		return cartesian1;
+	}
+
+	@Override
+	public double[] azimuth_elevation_dist(Point3D gps0, Point3D gps1) {
+
+		double[] my_azimuth_elevation_dist= new  double[3];
+		my_azimuth_elevation_dist[0] = gps1.north_angle(gps0);
+		my_azimuth_elevation_dist[1] = gps1.up_angle(gps0);		
+		my_azimuth_elevation_dist[2] = distance3d(gps1,gps0);
+		return my_azimuth_elevation_dist;
+	}
+
+	@Override
+	public boolean isValid_GPS_Point(Point3D p) 
+	{
+		return (p.x()<-180 || p.x()>180 || p.y()<-90 || p.y()>90 || p.z()<-450) ? false : true;
+	}
+}
---
+++ task_2/src/Coords/test.java
@@ -0,0 +1,9 @@
+package Coords;
+
+public class test {
+
+	public static void main(String[] args) {
+		MyCoords mycroods = new MyCoords();
+	}
+
+}
